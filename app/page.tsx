c101d1eb0145d40f05dadaaace7815a0f2c827b7
Shan Valdez <hisean.valdez@gmail.com>
2023-05-15 22:45:29 +0800

    Update main page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <p className="mb-3 text-6xl font-semibold">We&apos;ll be back soon.</p>
+      <p className="mb-3 text-6xl font-semibold">See you soon.</p>
     </main>
   );
 }
