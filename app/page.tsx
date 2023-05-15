d364557e008479d044a15041901e3c1aad21ba23
Shan Valdez <hisean.valdez@gmail.com>
2023-05-15 22:53:50 +0800

    Fix main page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <p className="mb-3 text-6xl font-semibold">See you soon.</p>
+    <main
+      className="
+        flex
+        min-h-screen
+        flex-col
+        items-center
+        justify-center
+        p-24
+      "
+    >
+      <p
+        className="
+          mb-3
+          text-3xl
+          font-semibold
+          md:text-4xl
+          lg:text-5xl
+          xl:text-6xl
+        "
+      >
+        See you soon.
+      </p>
     </main>
   );
 }
